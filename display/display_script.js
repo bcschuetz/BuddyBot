666870827a9ac4ec984c87c708e1e44428bdb66a
User <ulllk@student.kit.edu>
2024-11-22 16:33:55 +0100

    delete alerts

--- display/display_script.js
+++ display/display_script.js
@@ -6,11 +6,8 @@ let cycleThroughEvents = true;
 
 
 function getInfo(username, key) {
-    alert("0");
     var dataArray = document.getElementById("php-get-info").textContent;
-    alert("1");
     //var dataArray = div.textContent;
-    alert("hello");
     alert(dataArray);
 }
 
