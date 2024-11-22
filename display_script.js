6a9ff138ff8547a5cd1e21c43c3aad3799650fa6
ubxft <ubxft@student.kit.edu>
2024-11-22 09:31:37 +0100

    Add all files of the display website

---
+++ display_script.js
@@ -0,0 +1,26 @@
+let delayMilliseconds = 2000;
+
+let counter = 0;
+
+function next() {
+    counter++;
+
+    document.getElementById("header").innerText = "Header " + counter;
+    document.getElementById("description").innerText = "Description " + counter;
+    document.getElementById("image").src = counter % 2 == 1 ? "cat.jpeg" : "tac.jpeg";
+}
+
+function prev() {
+    counter--;
+
+    document.getElementById("header").innerText = "Header " + counter;
+    document.getElementById("description").innerText = "Description " + counter;
+    document.getElementById("image").src = counter % 2 == 1 ? "cat.jpeg" : "tac.jpeg";
+}
+
+function cycle() {
+    setInterval(next, delayMilliseconds);
+    next();
+}
+
+cycle();
