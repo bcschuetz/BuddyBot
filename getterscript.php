8e33255ad375541f7edfd4afee49ba8629536584
ubxft <ubxft@student.kit.edu>
2024-11-22 15:14:31 +0100

    php script

---
+++ getterscript.php
@@ -0,0 +1,7 @@
+<div id="php-get-info" style="display: none;">
+    <?php
+        $output = "42";
+        echo htmlspecialchars($output);
+    ?>
+</div>
+<script src="display_script.js"></script>
